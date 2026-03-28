1df3c5133d57956bec88ff654bf62ba4da46e151
SoumilLathey <soumil.lathey@gmail.com>
2026-03-28 17:48:17 +0530

    chore: delete root test files that cause Vercel build errors

--- test-db.ts
+++
@@ -1,2 +0,0 @@
-import { dbAll } from './src/lib/db2';
-dbAll('SELECT * FROM bookings').catch(e => console.error("Caught error:", e));
--- update-db.ts
+++
@@ -1,4 +0,0 @@
-import { dbRun, initDb } from './src/lib/db2';
-initDb().then(() => {
-  return dbRun("UPDATE users SET name='Admin', username='admin', email='admin@schedulo.app' WHERE id=1");
-}).then(() => console.log('DB Updated')).catch(console.error);
